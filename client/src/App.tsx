81d875294a97e0016ee1f06731637ec41f51a977
rajxode <rajsh1202@gmail.com>
2024-11-24 23:24:18 +0530

    completed basic ui with dummy data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,182 @@
-import { useState } from 'react'
+import { useState } from "react";
+import AddTodoDialog from "./components/AddTodoDialog";
+import EditTodoDialog from "./components/EditTodoDialog";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface todo {
+  id:number;
+  name: string;
+  status:string;
+  dueDate:string;
+  priority:string;
 }
 
-export default App
+let todos:Array<todo> = [
+  {
+    "id": 1,
+    "name": "Buy groceries",
+    "status": "pending",
+    "dueDate": "2024-11-25",
+    "priority": "high"
+  },
+  {
+    "id": 2,
+    "name": "Finish project report",
+    "status": "not done",
+    "dueDate": "2024-11-30",
+    "priority": "high"
+  },
+  {
+    "id": 3,
+    "name": "Walk the dog",
+    "status": "fullfilled",
+    "dueDate": "2024-11-23",
+    "priority": "medium"
+  },
+  {
+    "id": 4,
+    "name": "Attend team meeting",
+    "status": "pending",
+    "dueDate": "2024-11-25",
+    "priority": "low"
+  },
+  {
+    "id": 5,
+    "name": "Call mom",
+    "status": "fullfilled",
+    "dueDate": "2024-11-22",
+    "priority": "medium"
+  },
+  {
+    "id": 6,
+    "name": "Complete online course",
+    "status": "not done",
+    "dueDate": "2024-12-01",
+    "priority": "high"
+  },
+  {
+    "id": 7,
+    "name": "Clean the house",
+    "status": "pending",
+    "dueDate": "2024-11-25",
+    "priority": "low"
+  },
+  {
+    "id": 8,
+    "name": "Read new book",
+    "status": "not done",
+    "dueDate": "2024-12-10",
+    "priority": "medium"
+  },
+  {
+    "id": 9,
+    "name": "Prepare dinner",
+    "status": "fullfilled",
+    "dueDate": "2024-11-23",
+    "priority": "medium"
+  },
+  {
+    "id": 10,
+    "name": "Organize email inbox",
+    "status": "pending",
+    "dueDate": "2024-11-28",
+    "priority": "low"
+  }
+]
+
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [open, setOpen] = useState(false);
+  const [openEdit, setOpenEdit] = useState(false);
+  const [todoList, setTodoList] = useState<Array<todo>>(todos);
+  const [editTodo, setEditTodo] = useState<todo>({
+    id:-1,
+    name:"",
+    status:"pending",
+    dueDate:"1979-01-01",
+    priority:"low"
+  });
+
+  const handleEditClick = (i:number) => {
+    setEditTodo(todos[i]);
+    setOpenEdit(true);
+  }
+
+  const handleDeleteClick = (i:number) => {
+    let newTodos = todoList.filter((_,index) => index != i);
+    setTodoList(newTodos);
+  }
+
+  return (
+    <div className="w-full min-h-screen bg-gray-200 px-4 sm:px-[5%] md:px-[10%] py-[5vh]">
+      {/* search bar */}
+      <div className="w-full shadow bg-white rounded-md flex justify-around py-[10px]">
+        <div className="flex items-center">
+          <div className="mr-3 text-blue-500">My Todo</div>
+          <div className="w-auto border-2 border-blue-300 px-3 rounded-full">
+            <span className="mr-2 text-blue-400">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </span>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search..."
+              className="focus:outline-none py-1"
+            />
+            {search.length > 10 && (
+              <span
+                className="ml-2 cursor-pointer text-red-400"
+                onClick={() => setSearch("")}
+              >
+                <i className="fa-regular fa-circle-xmark"></i>
+              </span>
+            )}
+          </div>
+        </div>
+        <div>
+          <button 
+          onClick={(e) => setOpen(true)}
+            className="px-3 py-1 bg-blue-400 rounded-md text-white shadow hover:border hover:border-blue-400 
+            hover:bg-white hover:text-blue-400"
+          >
+            Add Todo
+          </button>
+        </div>
+      </div>
+
+      {/* todo list */}
+      <div className="w-full mt-[10vh] bg-white rounded-md shadow px-4 py-4">
+          {
+            todoList.map((todo,i) => (
+              <div key={i} className="w-full bg-gray-200 flex justify-between px-4 py-2 items-center rounded shadow-md mb-3">
+                  <div className="text-start flex-1">{i + 1}</div>
+                  <div className="text-start flex-1">{todo.name}</div>
+                  <div className="text-start flex-1">{todo.status}</div>
+                  <div className="text-start flex-1">{todo.dueDate}</div>
+                  <div className="text-start flex-1">{todo.priority}</div>
+                  <div>
+                    <span 
+                      onClick={() => handleEditClick(i)}
+                      className="mr-3 text-blue-400 cursor-pointer"
+                    >
+                      <i className="fa-regular fa-pen-to-square"></i>
+                    </span>
+                    <span 
+                      onClick={() => handleDeleteClick(i)}
+                      className="text-red-500 cursor-pointer"
+                    >
+                      <i className="fa-solid fa-trash"></i>
+                    </span>
+                  </div>
+              </div>
+            ))
+          }
+      </div>
+      <AddTodoDialog open={open} setOpen={setOpen} />
+      <EditTodoDialog open={openEdit} setOpen={setOpenEdit} todo={editTodo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/AddTodoDialog.tsx
@@ -0,0 +1,100 @@
+
+import React from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+
+interface props {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AddTodoDialog: React.FC<props> = ({ open, setOpen }) => {
+  return (
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 
+            data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+      />
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <DialogPanel
+            transition
+            className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all 
+                data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 
+                data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 
+                data-[closed]:sm:scale-95"
+          >
+            <div className="w-full bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div className="w-full sm:flex sm:items-start">
+                <div className="w-full mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h3"
+                    className="text-xl font-semibold text-gray-700 underline underline-offset-4"
+                  >
+                    Add Todo
+                  </DialogTitle>
+                  <div className="mt-2 flex flex-col w-full">
+                    <div className="w-full flex justify-between items-center">
+                      <label htmlFor="todoName" className="mr-3">Todo: </label>
+                      <input id="todoName" type="text" placeholder="todo.." className="w-full focus:outline-none border-b border-blue-400" />
+                    </div>
+
+                    <div className="w-full flex justify-between items-center mt-4">
+                      <label htmlFor="todoStatus" className="mr-3">Status: </label>
+                      <select id="todoStatus" className="w-full focus:outline-none border-b border-blue-400 pb-1">
+                        <option>Pending</option>
+                        <option>Fullfilled</option>
+                        <option>Failed</option>
+                      </select>
+                    </div>
+
+                    <div className="w-full flex justify-between items-center mt-4">
+                      <label htmlFor="todoPriority" className="mr-3">Priority: </label>
+                      <select id="todoPriority" className="w-full focus:outline-none border-b border-blue-400 pb-1">
+                        <option>Low</option>
+                        <option>Medium</option>
+                        <option>Hight</option>
+                      </select>
+                    </div>
+
+                    <div className="w-full flex justify-start items-center mt-4">
+                      <label htmlFor="todoDueDate" className="mr-3">Due Date: </label>
+                      <input id="todoDueDate" type="date" className="w-auto focus:outline-none border-b border-blue-400" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                type="button"
+                onClick={() => setOpen(false)}
+                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold 
+                    text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+              >
+                Cancel
+              </button>
+              <button
+                type="button"
+                data-autofocus
+                onClick={() => setOpen(false)}
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold 
+                    text-blue-500 shadow-sm ring-1 ring-inset ring-blue-400 hover:bg-blue-50 sm:mt-0 sm:w-auto"
+              >
+                Add
+              </button>
+            </div>
+          </DialogPanel>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default AddTodoDialog;
